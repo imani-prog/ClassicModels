764c81f2263bbb65d4ace2d9627597c8adacbe2c
imani-prog <timothyimani128@gmail.com>
2025-07-07 22:05:09 +0300

    Updated customer controller

--- src/main/java/com/classicmodels/classicmodels/controllers/CustomerController.java
+++ src/main/java/com/classicmodels/classicmodels/controllers/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/customers")
 public class
 CustomerController {
     private final CustomerService customerService;
